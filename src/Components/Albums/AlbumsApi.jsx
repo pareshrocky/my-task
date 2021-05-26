91421b0e2ece11016da0ad6bb391f9db2e05f472
paresh Sahoo <pareshrockyphry20002016@gmail.com>
2021-05-26 14:42:42 +0530

    wrong pagination insert

--- src/Components/Albums/AlbumsApi.jsx
+++ src/Components/Albums/AlbumsApi.jsx
@@ -1,46 +1,66 @@
 import React, { useState, useEffect } from "react";
- // eslint-disable-next-line
-import { Link, Switch, Route } from "react-router-dom";
-import PhotosApi from "./PhotosApi.jsx";
+import ReactPaginate from "react-paginate";
+import { Link } from "react-router-dom";
+import PhotosApi from "./PhotosApi";
 const AlbumsApi = () => {
   const [albums, setAlbums] = useState([]);
   const [albumPreview, setAlbumPreview] = useState(false);
   const [uid, setUid] = useState(0);
+  const [pageNumber, setPageNumber] = useState(0); //which page we are inside;
+  const albumsPerPage = 10; // contents per page =10;
+  const albumsVisited = pageNumber * albumsPerPage; // if 4 is the pg no. then 40 articles viewed;
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/albums`)
       .then((response) => response.json())
       .then((json) => setAlbums(json));
   }, []);
+  const displayArticles = albums
+    .slice(albumsVisited, albumsVisited + albumsPerPage)
+    .map((album) => {
+      let albumLink = "/albums/" + album.id;
+      return (
+        <div key={album.id}>
+          <li className="my-4 text-left">
+            <Link
+              onClick={() => {
+                setAlbumPreview((prev) => !prev);
+                setUid(album.id);
+              }}
+              to={albumLink}
+            >
+              {album.title}
+            </Link>
+          </li>
+        </div>
+      );
+    });
   return albumPreview === false ? (
-    <div className="container bg-light">
+    <div className="container bg-light pb-2">
       <h1 className="font-weight-light text-secondary py-3">Albums</h1>
       <h4 className="title-album font-weight-light text-secondary text-left my-3 mx-3">
         Album titles
       </h4>
       <ul className="text-secondary">
-        {albums.map((album) => {
-          let albumLink = "/albums/" + album.id;
-          return (
-            <div key={album.id}>
-              <li className="my-4 text-left">
-                <Link
-                  onClick={() => {
-                    setAlbumPreview((prev) => !prev);
-                    setUid(album.id);
-                  }}
-                  to={albumLink}
-                >
-                  {album.title}
-                </Link>
-              </li>
-              {/* <Switch>
-                <Route path={albumLink}>
-                  <PhotosApi id={album.id} />
-                </Route>
-              </Switch> */}
-            </div>
-          );
-        })}
+        {displayArticles}
+        <ReactPaginate
+          previousLabel="Previous"
+          nextLabel="Next"
+          pageCount={Math.ceil(albums.length / albumsPerPage)}
+          onPageChange={(data) => {
+            let selected = data.selected;
+            setPageNumber(selected);
+          }}
+          onPageActive={() => {
+            setPageNumber(1);
+          }}
+          containerClassName="paginationClass"
+          activeClassName="paginationActive"
+          previousLinkClassName="previousBtn"
+          nextLinkClassName="nextBtn"
+          pageLinkClassName="numerBtn"
+          pageRangeDisplayed={5}
+          marginPagesDisplayed={2}
+        />
       </ul>
     </div>
   ) : (
